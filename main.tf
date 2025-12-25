b705daa2cb10d5374f93fff6b9e63aeb599f5e66
unknown <ishwarjeet.s@outlook.com>
2025-12-25 13:17:49 +0530

    vnet addition

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg1" {
     name =     "rg_lake"
     location = "Central India"
 }
+
+resource "azurerm_virtual_network" "vnet_river" {
+name = "vnet_lake"
+address_space = [ "10.0.0.0/16" ]
+location = azurerm_resource_group.rg1.location
+resource_group_name = azurerm_resource_group.rg1.name
+}
